dc5d7268fef49278b032a624a16dc85ce439df28
Julia Yang <47124716+julia-yang@users.noreply.github.com>
2023-08-05 23:59:58 -0400

    add pharm_classes map

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from wtforms import Form, StringField, SelectField
 class DrugSearchForm(Form):
     choices = [('Name', 'Name'),
-               ('Classification', 'Classification')]
+               ('Clinical Category', 'Clinical Category')]
     select = SelectField('', choices=choices)
     search = StringField('', render_kw={"placeholder": "Enter Search Terms"})
